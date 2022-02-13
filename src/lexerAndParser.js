95d790d9c08a7c0e4e80f236ad69729985606346
SpongeBed <cmodz7969@gmail.com>
2022-02-13 21:26:26 +0300

    first

---
+++ src/lexerAndParser.js
@@ -0,0 +1,79 @@
+const fs = require("fs")
+
+let variables = [
+
+
+
+]
+
+//the variables are hopeless like a person. Variables can't control themselves they are controlled by us and us are controlled by other people
+
+
+const instructions = [
+    {
+        name: "cryLoud",
+        process: "console.log(argument)",
+        description: "crying endlessly is not an option"
+    },
+    {
+        name: "create",
+        process: `variables.push({owner: argument, value: argument})`,
+        description: "values are hopeless like us"
+    }
+]
+
+
+
+fs.readFile('main.hl', {
+        encoding: 'utf8',
+        flag: 'r'
+    },
+    function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            function exec() {
+                const SplitIntoChunks = data.split("\n")
+                SplitIntoChunks.forEach(element => {
+                    const getIndex = element.indexOf("=>")
+                    const getName = element.substring(0, getIndex).trim().split(" ")
+                     const findObject = instructions.find(el => el.name == getName[0])
+                     if(findObject !== undefined) {
+                         let getArgument = element.slice(getIndex+2).trim()
+                         if(findObject.process.includes("owner")) {
+                             if(getArgument.startsWith('"') && getArgument.endsWith('"')) {
+                                 getArgument = getArgument.slice(1)
+                                 getArgument = getArgument.substring(0, getArgument.length-1)
+                             }
+                             if(!isNaN(getArgument)) {
+                                 getArgument = Number(getArgument)
+                             }
+                             variables.push({owner: getName[1], value: getArgument})
+                         } else {
+                            let getProcess = findObject.process.replace("argument", getArgument)
+                            try{
+                                Function("return " + getProcess)()
+                            } catch(e) {
+                                if(e.message.includes("is not defined")) {
+                                    const sliceIt = e.message.substring(0, e.message.length-14).trim()
+                                    const findIt = variables.find(el => el.owner == sliceIt)
+                                    if(findIt !== undefined) {
+                                        let last;
+                                        if(!isNaN(getArgument)) {
+                                            getProcess = getProcess.replace(getArgument, Number(findIt.value))
+                                        } else {
+                                           getProcess =  getProcess.replace(getArgument, '"' + findIt.value + '"')
+                                        }
+                                        Function("return " + getProcess)()                                        
+                                    }
+                                }
+                            }
+                         }
+    
+                     }
+                });
+            }
+          
+            exec()
+         }
+    });
