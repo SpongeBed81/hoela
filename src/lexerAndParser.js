8ee1e5d66faf13732e38304c1f4b1661d11a0d14
SpongeBed <cmodz7969@gmail.com>
2022-02-14 17:21:27 +0300

    added killing and array support

--- src/lexerAndParser.js
+++ src/lexerAndParser.js
@@ -1,5 +1,6 @@
 const fs = require("fs")
-
+const nodeEval = require('node-eval');
+let rn = null
 let variables = [
 
 ]
@@ -7,8 +8,7 @@ let variables = [
 //the variables are hopeless like a person. Variables can't control themselves they are controlled by us and us are controlled by other people
 
 
-const instructions = [
-    {
+const instructions = [{
         name: "cryLoud",
         process: "console.log(argument)",
         description: "crying endlessly is not an option"
@@ -26,7 +26,18 @@ const instructions = [
                     find.value = argument
                 }
                 `,
-        description: "idk"        
+        description: "idk"
+    },
+    {
+        name: "kill",
+        process: `
+        const find = variables.find(el => el.owner == argument)
+        if(find !== undefined) {
+            const getIndex2 = variables.indexOf(find)
+            variables.splice(getIndex2, 1)
+        }
+        `,
+        description: "the variables goes to hell lmao"
     }
 ]
 
@@ -45,58 +56,76 @@ fs.readFile('main.hl', {
                 SplitIntoChunks.forEach(element => {
                     const getIndex = element.indexOf("=>")
                     const getName = element.substring(0, getIndex).trim().split(" ")
-                     const findObject = instructions.find(el => el.name == getName[0])
-                     if(findObject !== undefined) {
-                        let arguments = element.slice(getIndex+2).trim().split(" ")
-                        
+                    const findObject = instructions.find(el => el.name == getName[0])
+                    if (findObject !== undefined) {
+                        let arguments = element.slice(getIndex + 2).trim().split(" ")
 
-                         let getArgument = element.slice(getIndex+2).trim()
-                         if(findObject.process.includes("owner: argument")) {
-                             if(getArgument.startsWith('"') && getArgument.endsWith('"')) {
-                                 getArgument = getArgument.slice(1)
-                                 getArgument = getArgument.substring(0, getArgument.length-1)
-                             }
-                             if(!isNaN(getArgument)) {
-                                 getArgument = Number(getArgument)
-                             }
-                             variables.push({owner: getName[1], value: getArgument})
-                         } else if(findObject.process.includes("const find = variables.find(el => el.owner == argument)")) {
-                            if(getArgument.startsWith('"') && getArgument.endsWith('"')) {
+
+                        let getArgument = element.slice(getIndex + 2).trim()
+                        if (findObject.process.includes("owner: argument")) {
+                            if (getArgument.startsWith('"') && getArgument.endsWith('"')) {
                                 getArgument = getArgument.slice(1)
-                                getArgument = getArgument.substring(0, getArgument.length-1)
+                                getArgument = getArgument.substring(0, getArgument.length - 1)
                             }
-                            if(!isNaN(getArgument)) {
+                            if (!isNaN(getArgument)) {
+                                getArgument = Number(getArgument)
+                            }
+                            variables.push({
+                                owner: getName[1],
+                                value: getArgument
+                            })
+                        } else if (findObject.process.includes("const find = variables.find(el => el.owner == argument)") && !findObject.process.includes("getIndex2")) {
+                            if (getArgument.startsWith('"') && getArgument.endsWith('"')) {
+                                getArgument = getArgument.slice(1)
+                                getArgument = getArgument.substring(0, getArgument.length - 1)
+                            }
+                            if (!isNaN(getArgument)) {
                                 getArgument = Number(getArgument)
                             }
                             const find = variables.find(el => el.owner == getName[1])
-                            if(find !== undefined) {
+                            if (find !== undefined) {
                                 find.value = getArgument
                             }
-                         } else {
+                        } else {
                             let getProcess = findObject.process.replace("argument", getArgument)
-                            try{
-                                Function("return " + getProcess)()
-                            } catch(e) {
-                                if(e.message.includes("is not defined")) {
-                                    const sliceIt = e.message.substring(0, e.message.length-14).trim()
+                            try {
+                                if(getProcess.includes("splice")) {
+                                    const find = variables.find(el => el.owner == getArgument)
+                                    if(find !== undefined) {
+                                        const getIndex2 = variables.indexOf(find)
+                                        variables.splice(getIndex2, 1)
+                                    }
+                                } else {
+                                    nodeEval(getProcess)
+                                }
+                            } catch (e) {
+                                if (e.message.includes("is not defined")) {
+                                    const sliceIt = e.message.substring(0, e.message.length - 14).trim()
                                     const findIt = variables.find(el => el.owner == sliceIt)
-                                    if(findIt !== undefined) {
+                                    if (findIt !== undefined) {
                                         let last;
-                                        if(!isNaN(getArgument)) {
+                                        if (!isNaN(getArgument)) {
                                             getProcess = getProcess.replace(getArgument, Number(findIt.value))
                                         } else {
-                                           getProcess =  getProcess.replace(getArgument, '"' + findIt.value + '"')
+                                            try{
+                                                rn = JSON.parse(findIt.value)
+                                            } catch(e) {}
+                                            if(Array.isArray(rn)) {
+                                                getProcess = getProcess.replace(getArgument, findIt.value)
+                                            } else {
+                                                getProcess = getProcess.replace(getArgument, '"' + findIt.value + '"')
+                                            }
                                         }
-                                        Function("return " + getProcess)()          
+                                        nodeEval(getProcess)
                                     }
                                 }
                             }
-                         }
-    
-                     }
+                        }
+
+                    }
                 });
             }
-          
+
             exec()
-         }
+        }
     });
